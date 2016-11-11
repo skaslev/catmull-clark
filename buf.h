6ccba071721b23c55bc1c6f47537f8a5226b4426
Slavomir Kaslev <slavomir.kaslev@wwfx.net>
2016-11-11 17:10:15 +0200

    Changed buf.h memory growth to 1.5.

--- buf.h
+++ buf.h
@@ -8,6 +8,7 @@
  */
 #define buf_len(a)		((a) ? buf_n_(a) : 0)
 #define buf_push(a, v)		(buf_maybegrow1_(a), (a)[buf_n_(a)++] = (v))
+#define buf_last(a)		((a)[buf_n_(a) - 1])
 #define buf_resize(a, n)	(buf_maybegrow_(a, n), (a) ? buf_n_(a) = (n) : 0)
 #define buf_reserve(a, n)	(buf_maybegrow_(a, n))
 #define buf_free(a)		((a) ? free(buf_raw_(a)) : (void) 0)
@@ -20,7 +21,7 @@
 
 #define buf_maybegrow_(a, n)	(((n) > 0) && (!(a) || (n) >= buf_m_(a)) ? buf_realloc_(a, n) : (void) 0)
 #define buf_maybegrow1_(a)	(!(a) || buf_m_(a) == 0 ? buf_realloc_(a, 8) : \
-				 buf_n_(a) == buf_m_(a) ? buf_realloc_(a, 2 * buf_m_(a)) : (void) 0)
+				 buf_n_(a) == buf_m_(a) ? buf_realloc_(a, 3 * buf_m_(a) / 2) : (void) 0)
 #define buf_realloc_(a, n)	buf_do_realloc_((void **) &(a), n, sizeof(*(a)))
 
 void buf_do_realloc_(void **a, size_t nr, size_t sz);
